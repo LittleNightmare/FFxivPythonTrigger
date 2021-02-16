21ad83430d99df2b19c4ed74ad6a31e175049554
nyaoouo <kikiandchch@gmail.com>
2021-02-16 17:32:31 +0800

    update*

--- plugin/FFxivMemory/ValToBytes.py
+++
@@ -1,9 +0,0 @@
-from binascii import unhexlify
-
-
-def long_to_bytes(val, little_endianness=False):
-    width = val.bit_length()
-    width += 8 - ((width % 8) or 8)
-    fmt = '%%0%dx' % (width // 4)
-    s = unhexlify(fmt % val)
-    return s[::-1] if little_endianness else s
